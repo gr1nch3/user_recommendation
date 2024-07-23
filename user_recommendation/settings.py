b6ee812689a0ebf0d041dee8974e1d4da5b07522
gr1nch3 <gr1nch3@proton.me>
2024-07-23 18:40:57 +0200

    changed up conection

--- user_recommendation/settings.py
+++ user_recommendation/settings.py
@@ -94,9 +94,14 @@ WSGI_APPLICATION = "user_recommendation.wsgi.application"
 # }
 
 DATABASES = {
-    "default": env.db(
-        "POSTGRES_URL", default="postgres://postgres:insecure-postgres@db:5432/recdb"
-    )
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("POSTGRES_DB"),
+        "USER": env.str("POSTGRES_USER"),
+        "PASSWORD": env.str("POSTGRES_PASSWORD"),
+        "HOST": env.str("POSTGRES_HOST"),
+        "PORT": env.int("POSTGRES_PORT"),
+    }
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
